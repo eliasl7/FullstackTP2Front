5f82d36629d1b107c877400270b37fcf772a4d5e
Elias LARBI <elias.larbi@univ-rouen.fr>
2025-01-19 13:04:16 +0100

    feat(artists): add artists module with routing

---
+++ src/app/features/artists/artists-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ArtistsListComponent } from './components/artists-list/artists-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ArtistsListComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ArtistsRoutingModule {}
